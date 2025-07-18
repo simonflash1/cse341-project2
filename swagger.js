787acc8e4eeecbad2f044d26409a831c3e814a5b
simonflash1 <nestor.emmanuel.lizarraga@gmail.com>
2025-07-18 19:47:00 -0300

    swagger adjustment

--- swagger.js
+++ swagger.js
@@ -2,11 +2,11 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Temple API',
+    title: 'Rockband API',
+    description: 'Project 2 API',
   },
   host: 'localhost:8080',
-  schemes: ['http'],
+  schemes: ['http', 'https'],
 };
 
 const outputFile = './routes/swagger.json';
